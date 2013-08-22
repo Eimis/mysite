4dfba2d18ddbdbd1c7eae9ee867ffc3b2095652b
Eimis <eimantas.stonys@gmail.com>
2013-08-22 23:13:09 +0300

    Aug. 22

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -16,7 +16,3 @@ def search(request):
 			books = Book.objects.filter(title__icontains=q)
 			return render(request, "search_results.html", {"books" : books, "query": q})
 	return render(request, "search_form.html", {"errors" : errors}) #wintest_last
-
-def contact(request):
-	errors = []
-	
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from mysite.views import hello, current_datetime, hours_ahead, display_meta
 from mysite.books import views
+from mysite.contact.views import contact, thanks
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,6 +14,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^display_meta/$', display_meta),
     url(r'^search/$', views.search),
+    url(r'^contact/$', contact),
+    url(r'^contact/thanks/$', thanks),
 
 
 )
